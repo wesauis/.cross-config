f26b7a3d07601ce506c1eafdc4b5570d88674d4d
Wesley Augusto Isotton <54046750+wesauis@users.noreply.github.com>
2022-02-21 11:53:09 -0300

    cli: install fzf

--- setup/cli.sh
+++ setup/cli.sh
@@ -1,3 +1,7 @@
 info? installing jq $(dim 'https://stedolan.github.io/jq/')
 sudo apt install -y jq 2>>$logfile 1>&2 &
 wait_job
+
+info? installing fzf $(dim 'https://github.com/junegunn/fzf')
+sudo apt install -y fzf 2>>$logfile 1>&2 &
+wait_job
